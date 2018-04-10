6a82c5bb9ac6fc683ea1d98585e1c75484295f9f
lfrancois <liesbeth.francois@ucb.com>
2018-04-10 16:04:56 +0200

    Initial commit

---
+++ scripts/S01-Extract-sources.R
@@ -0,0 +1,43 @@
+setwd("~/Shared/Data-Science/Data-Source-Model-Repository/DO/scripts/")
+
+library(git2r)
+library(RJSONIO)
+
+desc <- readJSONStream("../DESCRIPTION.json")
+
+sourceFiles <- desc$"source files"
+urls <- unlist(lapply(
+  sourceFiles,
+  function(sf){
+    toRet <- sf$"URL template"
+    names(toRet) <- sf$"name"
+    return(toRet)
+  }
+))
+srcDir <- "../sources/DO"
+gitRepo <- urls[1]
+
+## Clone or pull git repository
+if(!dir.exists(srcDir)){
+  gitRepo <- git2r::clone(url = gitRepo, local_path = srcDir)
+}else{
+  gitRepo <- git2r::repository(srcDir)
+  git2r::pull(gitRepo)
+}
+
+###############################################
+## Information source files
+rcurrent <- git2r::odb_blobs(gitRepo)
+rcurrent <- tail(rcurrent[rcurrent$name == "doid.json",], n = 1L)
+
+DO_sourceFiles <- data.frame(url = urls,
+                             current = rcurrent$when)
+
+###############################################
+## Writing files
+toSave <- grep("^DO[_]", ls(), value = T)
+ddir <- "../data"
+
+write.table(toSave, file=file.path(ddir, paste(toSave, ".txt", sep="")))
+
+
---
+++ scripts/S02-Process-Sources.R
@@ -0,0 +1,144 @@
+setwd("~/Shared/Data-Science/Data-Source-Model-Repository/DO/scripts/")
+
+library(XML)
+library(parallel)
+library(git2r)
+library(RJSONIO)
+
+##
+mc.cores <- 55
+sdir <- "../sources/DO/src/ontology/"
+ddir <- "../data"
+
+###############################################################################@
+## Source information ----
+###############################################################################@
+
+desc <- RJSONIO::readJSONStream("../DESCRIPTION.json")
+
+sourceFiles <- desc$"source files"
+sfi_name <- unlist(lapply(
+  sourceFiles,
+  function(sf){
+    toRet <- sf$"name"
+    return(toRet)
+  }
+))
+
+###############################################################################@
+## Data from doid.json ----
+###############################################################################@
+readJson <- jsonlite::fromJSON(txt = file.path(sdir,"doid.json"))
+
+###########################################
+## nodes (id, def, name, xref, label)
+nodesJson <- do.call(rbind,
+                     lapply(1:nrow(readJson$graphs$nodes[[1]]),
+                            function(i){
+                              ## id
+                              id <- gsub("_",":",gsub(".*/","",readJson$graphs$nodes[[1]]$id[[i]]))
+                              ## def
+                              def <- readJson$graphs$nodes[[1]]$meta$definition[i, "val"]
+                              ## Xref
+                              Xref <- paste(readJson$graphs$nodes[[1]]$meta$xrefs[[i]]$val,collapse = ", ")
+                              ## name
+                              name <- paste(readJson$graphs$nodes[[1]]$meta$synonyms[[i]]$val,collapse = ", ")
+                              ## lbl
+                              lbl <- readJson$graphs$nodes[[1]]$lbl[[i]]
+                              ## df
+                              df <- data.frame(
+                                id = id,
+                                Xref = Xref,
+                                def = def,
+                                name = name,
+                                label = lbl,
+                                stringsAsFactors = FALSE)
+                              return(df)
+                            }
+                     )
+)
+
+## edges (parents)
+edgesJson <- readJson$graphs$edges[[1]]
+edgesJson <- edgesJson[which(edgesJson$pred %in% c("is_a")),]
+edgesJson$sub <- gsub("_",":",gsub(".*/","",edgesJson$sub))
+edgesJson$obj <- gsub("_",":",gsub(".*/","",edgesJson$obj))
+
+######################################
+## crossId
+crossId <- unique(nodesJson[,c("id","Xref")])
+crossIdList <- strsplit(crossId$Xref, split = ",")
+names(crossIdList) <- crossId$id
+crossId <- stack(crossIdList)
+names(crossId) <- c("id2","id1")
+crossId$id2 <- gsub("MESH","MeSH",crossId$id2)
+crossId$id2 <- gsub("NCiT","NCIt",crossId$id2)
+crossId$id2 <- gsub("NCI","NCIt",crossId$id2)
+crossId$id2 <- gsub("ORDO","ORPHA",crossId$id2)
+crossId$id2 <- gsub("UMLS_CUI","MedGen",crossId$id2)
+crossId$id2 <- gsub("UMLS","MedGen",crossId$id2)
+crossId$DB2 <- gsub(":.*","",crossId$id2)
+crossId$DB1 <- gsub(":.*","",crossId$id1)
+
+######################################
+## entryId
+entryId <- crossId[!duplicated(crossId$id1),c("DB1","id1")]
+names(entryId) <- c("DB","id")
+entryId$definition <- nodesJson$def[match(entryId$id,nodesJson$id)]
+
+######################################
+## idNames
+idNames <- unique(nodesJson[,c("id","name")])
+idNamesList <- strsplit(idNames$name, split = ",")
+names(idNamesList) <- idNames$id
+idNames <- stack(idNamesList)
+names(idNames) <- c("name","id")
+## Labels
+lbl <- unique(nodesJson[,c("label","id")])
+## 
+idNames <- rbind(idNames,setNames(lbl, nm = names(idNames)))
+idNames$DB <- gsub(":.*","",idNames$id)
+idNames$canonical <- ifelse(idNames$name %in% lbl$label, TRUE, FALSE)
+
+######################################
+## parentId
+parentId <- edgesJson[,c("sub","obj")]
+names(parentId) <- c("id","parent")
+parentId$DB <- gsub(":.*","",parentId$id)
+parentId$pDB <- gsub(":.*","",parentId$parent)
+
+#######################################
+crossId$id1 <- gsub(".*:","",crossId$id1)
+crossId$id2 <- gsub(".*:","",crossId$id2)
+entryId$id <- gsub(".*:","",entryId$id)
+parentId$id <- gsub(".*:","",parentId$id)
+parentId$parent <- gsub(".*:","",parentId$parent)
+idNames$id <- gsub(".*:","",idNames$id)
+
+############################
+DO_idNames <- idNames[,c("DB","id","name")]
+DO_parentId <- parentId[,c("DB","id","pDB","parent")]
+DO_crossId <- crossId[,c("DB1","id1","DB2","id2")]
+DO_entryid <- entryId[,c("DB","id")]
+
+############################
+toSave <- grep("^DO[_]", ls(), value=T)
+for(f in toSave){
+  message(paste("Saving", f))
+  ## Ensure unicity
+  assign(f, get(f))
+  if(length(names(f))==0){
+    f <- unique(f)
+  }
+  ##
+  write.table(
+    get(f),
+    file=file.path(ddir, paste(f, ".txt", sep="")),
+    sep="\t",
+    row.names=FALSE, col.names=TRUE,
+    quote=FALSE
+  )
+}
+
+
+
